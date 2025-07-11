ccc55c8d655058e71473d7112b17b60ea7ede44b
DevGaga <raphael.chinyama@cs.unza.zm>
2025-07-11 18:41:18 +0200

    added logic in institutions

--- EduBridge/institution/views.py
+++ EduBridge/institution/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Institution
+from opportunities.models import Opportunity
+from opportunities.forms import OpportunityForm
+
+@login_required
+def dashboard(request):
+    institution = get_object_or_404(Institution, user=request.user)
+    opportunities = Opportunity.objects.filter(institution=institution)
+    return render(request, 'institutions/dashboard.html', {
+        'opportunities': opportunities
+    })
+
+@login_required
+def create_opportunity(request):
+    institution = get_object_or_404(Institution, user=request.user)
+
+    if request.method == 'POST':
+        form = OpportunityForm(request.POST)
+        if form.is_valid():
+            opp = form.save(commit=False)
+            opp.institution = institution
+            opp.save()
+            return redirect('institutions:dashboard')
+    else:
+        form = OpportunityForm()
+
+    return render(request, 'institutions/create_opportunity.html', {
+        'form': form
+    })
