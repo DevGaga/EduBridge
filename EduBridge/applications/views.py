8698cff59b7c057a82d4a3ec97329b0dc605cbcc
DevGaga <raphael.chinyama@cs.unza.zm>
2025-07-11 18:47:06 +0200

    added logic for applications

--- EduBridge/applications/views.py
+++ EduBridge/applications/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from .models import Application
+
+@login_required
+def application_detail(request, pk):
+    application = get_object_or_404(Application, pk=pk)
+    return render(request, 'applications/detail.html', {
+        'application': application
+    })
