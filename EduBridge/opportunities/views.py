74b2a38229c48dca5234c56b629073b2b19ece6f
DevGaga <raphael.chinyama@cs.unza.zm>
2025-07-11 18:43:51 +0200

    added logic in opportunities

--- EduBridge/opportunities/views.py
+++ EduBridge/opportunities/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Opportunity
+
+def public_list(request):
+    opportunities = Opportunity.objects.all().order_by('-created_at')
+    return render(request, 'opportunities/list.html', {
+        'opportunities': opportunities,
+    })
