bdcac573a8fda51f315eacb602bbb48c1714aac3
DevGaga <raphael.chinyama@cs.unza.zm>
2025-07-15 17:56:33 +0200

    changed models.py in institutions

--- EduBridge/institutions/models.py
+++ EduBridge/institutions/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from accounts.models import User
 
 class Institution(models.Model):
+    INSTITUTION_TYPES = [
+        ('university', 'University'),
+        ('college', 'College'),
+        ('company', 'Company'),
+        ('ngo', 'NGO'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    type = models.CharField(max_length=50, choices=INSTITUTION_TYPES)
     contact_email = models.EmailField()
     website = models.URLField(blank=True)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
