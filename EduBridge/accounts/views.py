7ab44544253645b424939f8941c0fe0046d7cb96
DevGaga <raphael.chinyama@cs.unza.zm>
2025-07-19 16:44:54 +0200

    updated views in accounts

--- EduBridge/accounts/views.py
+++ EduBridge/accounts/views.py
@@ -4,21 +4,32 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.views.generic import CreateView
 from django.urls import reverse
+
 from .models import User
 from .forms import StudentSignupForm, InstitutionSignupForm
 from students.models import StudentProfile
-from institutions.models import Institution
+from institutions.models import Institution  # if using Institution model
+
 
 # Account selection page
 class CustomLoginView(LoginView):
     template_name = 'accounts/create_account.html'
 
+
 # Login views
 class InstitutionLoginView(LoginView):
     template_name = 'accounts/institution_login.html'
 
+    def get_success_url(self):
+        return reverse('accounts:dashboard_redirect')
+
+
 class StudentLoginView(LoginView):
     template_name = 'accounts/student_login.html'
+
+    def get_success_url(self):
+        return reverse('accounts:dashboard_redirect')
+
 
 # Redirect to correct dashboard based on role
 @login_required
@@ -26,9 +37,10 @@ def dashboard_redirect(request):
     if request.user.role == 'student':
         return redirect('students:student_dashboard')
     elif request.user.role == 'institutions':
-        return redirect('institutions:dashboard')
+        return redirect('institutions:institution_dashboard')
     else:
         return redirect('admin:index')
+
 
 # Student signup view
 class StudentRegisterView(CreateView):
@@ -39,7 +51,8 @@ class StudentRegisterView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('students:student_dashboard')
+        return redirect('accounts:dashboard_redirect')
+
 
 # Institution signup view
 class InstitutionRegisterView(CreateView):
@@ -50,4 +63,4 @@ class InstitutionRegisterView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('institutions:dashboard')
+        return redirect('accounts:dashboard_redirect')
